6059ab000eaebc02b19f7a52d84378bd71ac0205
hotwatermorning <hotwatermorning@gmail.com>
2019-07-22 00:54:49 +0900

    [gui/Gui] fix menu positions and a command handler for Windows

--- ChapterXX/src/gui/Gui.cpp
+++ ChapterXX/src/gui/Gui.cpp
@@ -481,9 +481,6 @@ public:
         SetMinClientSize(wxSize(350, 600));
        
         auto menu_file = new wxMenu();
-        
-        menu_file->Append(wxID_ABOUT, L"&Vst3SampleHostについて...", L"Vst3SampleHostについて");
-        menu_file->Append(wxID_EXIT, L"&Quit\tCTRL-Q", L"アプリケーションを終了します");
 
         menu_file->Append(kID_File_Load, L"開く\tCTRL-O", L"プロジェクトファイルを開きます");
         menu_file->Append(kID_File_Save, L"保存\tCTRL-S", L"プロジェクトファイルを保存します");
@@ -508,12 +505,32 @@ public:
         auto menu_device = new wxMenu();
         menu_device->Append(kID_Device_Preferences, L"デバイス設定\tCTRL-,", L"デバイス設定を変更します");
         
+#if defined(_MSC_VER)
+        auto menu_help = new wxMenu();   
+        menu_help->Append(wxID_ABOUT, L"&Vst3SampleHostについて...", L"Vst3SampleHostについて");
+
+        menu_file->AppendSeparator();
+        menu_file->Append(wxID_EXIT, L"&Quit\tCTRL-Q", L"アプリケーションを終了します");
+#else
+        menu_file->Append(wxID_ABOUT, L"&Vst3SampleHostについて...", L"Vst3SampleHostについて");
+        menu_file->Append(wxID_EXIT, L"&Quit\tCTRL-Q", L"アプリケーションを終了します");
+#endif
+
         auto menubar = new wxMenuBar();
         menubar->Append(menu_file, L"ファイル");
         menubar->Append(menu_playback, L"再生");
         menubar->Append(menu_view, L"表示");
         menubar->Append(menu_device, L"デバイス");
+#if defined(_MSC_VER)
+        menubar->Append(menu_help, L"ヘルプ");
+#endif
         SetMenuBar(menubar);
+
+#if defined(_MSC_VER)
+        Bind(wxEVT_COMMAND_MENU_SELECTED, [this](auto&) {
+            Close();
+        }, wxID_EXIT);
+#endif
         
         Bind(wxEVT_COMMAND_MENU_SELECTED, [](auto &) {
             auto app = App::GetInstance();
