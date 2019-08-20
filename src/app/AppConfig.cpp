562e0823e27c6f0bd66450a5fb7ffa19b74e572b
hotwatermorning <hotwatermorning@gmail.com>
2019-08-21 03:04:34 +0900

    [cmake] refactor product info configuration

--- src/app/AppConfig.cpp
+++ src/app/AppConfig.cpp
@@ -2,8 +2,14 @@
 
 NS_HWM_BEGIN
 
-wchar_t const * const kAppName = L"Vst3SampleHost";
-wchar_t const * const kAppVersion = L"0.0.0.1";
+wchar_t const * const kAppName =
+#include "./AppName.inc"
+;
+
+wchar_t const * const kAppVersion =
+#include "./Version.inc"
+;
+
 wchar_t const * const kAppCommitID =
 #include "./CommitID.inc"
 ;
